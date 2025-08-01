5bb438e84cd91834af71a401434d9f14c589de5c
Hassannewcode <privatealkassan@gmail.com>
2025-08-01 11:39:01 +0300

    Create page-e778543c0e785604.js

---
+++ page-e778543c0e785604.js
@@ -0,0 +1,21 @@
+!function() {
+    try {
+        var e = "undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof self ? self : {}
+          , n = (new e.Error).stack;
+        n && (e._sentryDebugIds = e._sentryDebugIds || {},
+        e._sentryDebugIds[n] = "8013af5d-8ea0-4043-a208-7c5d3593c4b2",
+        e._sentryDebugIdIdentifier = "sentry-dbid-8013af5d-8ea0-4043-a208-7c5d3593c4b2")
+    } catch (e) {}
+}(),
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([[9852], {
+    74074: function(e, n, d) {
+        Promise.resolve().then(d.bind(d, 97844)),
+        Promise.resolve().then(d.bind(d, 37100))
+    }
+}, function(e) {
+    e.O(0, [3189, 5441, 7341, 5052, 4834, 5878, 7337, 7728, 6818, 5239, 1247, 5039, 5974, 8544, 5411, 7245, 9589, 9702, 7942, 6525, 3015, 5677, 7844, 1682, 7100, 2971, 9322, 1744], function() {
+        return e(e.s = 74074)
+    }),
+    _N_E = e.O()
+}
+]);
